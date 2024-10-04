ac0a696b6320b54d66e3ae9b713f3587755d2092
AkshatGKMit <akshat@gkmit.co>
2024-10-04 14:14:36 +0530

    feat: Add routes and navbar

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,13 +1,29 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { DashboardContextProvider } from "../../contexts/DashboardContext";
 import Dashboard from "../Dashboard/Dashboard";
 import "./App.scss";
+import Navbar from "../../components/Navbar/Navbar";
+import Page404 from "../Page404/Page404";
+import AddEditEvent from "../AddEditEvent/AddEditEvent";
 
 function App() {
     return (
         <div className="App">
-            <DashboardContextProvider>
-                <Dashboard />
-            </DashboardContextProvider>
+            <BrowserRouter>
+                <Navbar />
+                <Routes>
+                    <Route
+                        path="/"
+                        element={
+                            <DashboardContextProvider>
+                                <Dashboard />
+                            </DashboardContextProvider>
+                        }
+                    />
+                    <Route path="/add-edit-event/:eventId" Component={AddEditEvent} />
+                    <Route path="*" Component={Page404} />
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 }
